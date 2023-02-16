d1b5499106b417a99969e951ad1c851cea844f6c
DADA YO <dayeon.yoon@gmail.com>
2023-02-17 00:26:16 +0900

    STYLE: utterances dark theme

--- src/components/post/CommentWidget.tsx
+++ src/components/post/CommentWidget.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect } from "react";
+import { createRef, useEffect, useLayoutEffect, useState } from "react";
 
 const src = "https://utteranc.es/client.js";
 const repo = "COYO-HM/COYO-HM.github.io"; // 자신 계정의 레포지토리로 설정
@@ -15,28 +15,37 @@ interface UtterancesAttributesType {
 
 const CommentWidget = () => {
   const element = createRef<HTMLDivElement>();
+  const [theme, setTheme] = useState("");
+
+  useLayoutEffect(() => {
+    setTheme(localStorage.theme === "dark" ? "dark" : "light");
+  }, []);
 
   useEffect(() => {
-    if (element.current === null) return;
+    if (element.current === null || theme === "") {
+      return;
+    } else {
+      const utterances: HTMLScriptElement = document.createElement("script");
 
-    const utterances: HTMLScriptElement = document.createElement("script");
+      const attributes: UtterancesAttributesType = {
+        src,
+        repo,
+        "issue-term": "pathname",
+        label: "Comment",
+        theme: theme ? `photon-dark` : `github-light`,
+        crossorigin: "anonymous",
+        async: "true",
+      };
 
-    const attributes: UtterancesAttributesType = {
-      src,
-      repo,
-      "issue-term": "pathname",
-      label: "Comment",
-      theme: `github-light`,
-      crossorigin: "anonymous",
-      async: "true",
-    };
+      Object.entries(attributes).forEach(([key, value]) => {
+        utterances.setAttribute(key, value);
+      });
 
-    Object.entries(attributes).forEach(([key, value]) => {
-      utterances.setAttribute(key, value);
-    });
+      console.log(theme, utterances);
 
-    element.current.appendChild(utterances);
-  }, [element]);
+      element.current.appendChild(utterances);
+    }
+  }, [element, theme]);
 
   return <div ref={element} id={`utterances`} />;
 };
