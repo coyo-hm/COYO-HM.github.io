a560ffc2e13682d4a1e1a74712cfcd78439d8f96
DADA YO <dayeon.yoon@gmail.com>
2023-02-18 20:38:20 +0900

    FIX: Comment Style

--- src/components/post/CommentWidget.tsx
+++ src/components/post/CommentWidget.tsx
@@ -1,6 +1,4 @@
-import useTheme from "@hooks/useTheme";
-import Script from "next/script";
-import { createRef, useEffect, useLayoutEffect, useState } from "react";
+import { createRef, useEffect } from "react";
 
 const src = "https://utteranc.es/client.js";
 const repo = "COYO-HM/COYO-HM.github.io"; // 자신 계정의 레포지토리로 설정
@@ -16,53 +14,55 @@ interface UtterancesAttributesType {
 }
 
 const CommentWidget = () => {
-  const element = createRef<HTMLDivElement>();
-  const { isDarkTheme } = useTheme();
+  const elementLight = createRef<HTMLDivElement>();
+  const elementDark = createRef<HTMLDivElement>();
 
-  console.log(isDarkTheme);
-  const attributes: UtterancesAttributesType = {
-    src,
-    repo,
-    "issue-term": "pathname",
-    label: "Comment",
-    theme: isDarkTheme ? `photon-dark` : `github-light`,
-    crossorigin: "anonymous",
-    async: "true",
-  };
+  useEffect(() => {
+    if (elementLight.current === null || elementDark.current === null) {
+      return;
+    } else {
+      const utterances: HTMLScriptElement = document.createElement("script");
+      const utterancesDark: HTMLScriptElement =
+        document.createElement("script");
 
-  // useEffect(() => {
-  //   if (element.current === null) {
-  //     return;
-  //   } else {
-  //     const utterances: HTMLScriptElement = document.createElement("script");
+      const attributes: UtterancesAttributesType = {
+        src,
+        repo,
+        "issue-term": "pathname",
+        label: "Comment",
+        theme: `github-light`,
+        crossorigin: "anonymous",
+        async: "true",
+      };
 
-  //     const attributes: UtterancesAttributesType = {
-  //       src,
-  //       repo,
-  //       "issue-term": "pathname",
-  //       label: "Comment",
-  //       theme: isDarkTheme ? `photon-dark` : `github-light`,
-  //       crossorigin: "anonymous",
-  //       async: "true",
-  //     };
+      const darkAttributes: UtterancesAttributesType = {
+        src,
+        repo,
+        "issue-term": "pathname",
+        label: "Comment",
+        theme: `photon-dark`,
+        crossorigin: "anonymous",
+        async: "true",
+      };
 
-  //     Object.entries(attributes).forEach(([key, value]) => {
-  //       utterances.setAttribute(key, value);
-  //     });
+      Object.entries(attributes).forEach(([key, value]) => {
+        utterances.setAttribute(key, value);
+      });
 
-  //     console.log(utterances);
+      Object.entries(darkAttributes).forEach(([key, value]) => {
+        utterancesDark.setAttribute(key, value);
+      });
 
-  //     element.current.appendChild(utterances);
-  //   }
-  // }, [element, isDarkTheme]);
+      elementLight.current.appendChild(utterances);
+      elementDark.current.appendChild(utterancesDark);
+    }
+  }, [elementDark, elementLight]);
 
   return (
-    <div
-      ref={element}
-      id={isDarkTheme ? "utterances-dark" : "utterances-light"}
-    >
-      <Script {...attributes} />
-    </div>
+    <>
+      <div ref={elementLight} id={"utterances-light"} />
+      <div ref={elementDark} id={"utterances-dark"} />
+    </>
   );
 };
 
