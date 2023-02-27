9a002df74ab1078b059e531f2cbded135eab671c
DADA YO <dayeon.yoon@gmail.com>
2023-02-28 04:16:24 +0900

    STYLE: PostHeader Title

--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -7,7 +7,7 @@ const PostHeader = ({ title, date }: PostHeaderPropsType) => {
   const { dateStr } = getDate(date);
   return (
     <header className={`pt-28 pb-14 text-5xl font-semibold text-center`}>
-      <h1 className={`break-keep max-md:text-xl`}>{title}</h1>
+      <h1 className={`break-keep max-md:text-xl leading-snug`}>{title}</h1>
       <p className={`text-sm text-neutral-500 mt-10`}>{dateStr}</p>
     </header>
   );
