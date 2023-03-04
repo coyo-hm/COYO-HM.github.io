4a1b2b8c18c806ec81196a117701c78e1e9cd1e7
DADA YO <dayeon.yoon@gmail.com>
2023-03-05 04:23:23 +0900

    FIX: Post Tag Color

--- src/components/post/TagsList.tsx
+++ src/components/post/TagsList.tsx
@@ -7,11 +7,10 @@ const TagsList = ({ tags, slug: path }: { tags: string[]; slug: string }) => {
       <h1 className={`text-lg mr-3 mb-2`}>Tags</h1>
       {tags.map((tag) => {
         const tagHoverColor =
-          TAG_COLOR.BG_HOVER[tag] ||
-          "dark:bg-neutral-700 hover:bg-blue-700 dark:hover:bg-blue-700";
+          TAG_COLOR.BG_HOVER[tag] || "hover:bg-blue-700 dark:hover:bg-blue-700";
         return (
           <Link
-            className={`mr-2 mb-2 h-6 p-1 w-fit break-keep text-xs rounded bg-neutral-100 hover:text-white ${tagHoverColor}`}
+            className={`mr-2 mb-2 h-6 p-1 w-fit break-keep text-xs rounded bg-neutral-100 dark:bg-neutral-700 hover:text-white ${tagHoverColor}`}
             href={`/blog/tags/${tag}`}
             key={tag}
           >
