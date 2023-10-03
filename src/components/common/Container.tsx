e11daa869b7b9508c1f50c0db4dab714f6ba08fb
COYO <dayeon.yoon@gmail.com>
2023-10-04 00:21:16 +0900

    STYLE: light mode color fix
    라이트 모드 텍스트 컬러 모드 수정

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -20,7 +20,7 @@ const Container = (props: {
 }) => {
   return (
     <div
-      className={`bg-neutral-100 dark:bg-neutral-800 dark:text-neutral-100 min-w-[320px]`}
+      className={`bg-neutral-100 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-100 min-w-[320px]`}
     >
       <div
         className={`min-h-screen md:max-lg:w-full lg:w-[1000px] flex flex-col items-center px-8 m-auto dark:bg-neutral-800`}
