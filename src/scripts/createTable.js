9c3e7114934449abc16f306eb3d7e85f0d85d4e1
COYO <dayeon.yoon@gmail.com>
2023-11-12 22:16:52 +0900

    REFACTOR: createposttable -> createtable

--- src/scripts/createTable.js
+++ src/scripts/createTable.js
@@ -6,13 +6,33 @@ const frontMatter = require("front-matter");
 const CONTENT_DIR_PATH = path.join(process.cwd(), "content");
 const TABLE_DIR_PATH = path.join(process.cwd(), "public/static/table");
 
-const createPostTable = () => {
+const isIntroPost = (postKey = "") => {
+  const postDirectory = postKey.split("/");
+  const postDirLength = postDirectory.length;
+  const fileName = postDirectory[postDirLength - 1];
+  return fileName === "intro";
+};
+
+const getSeriesTitle = (postKey = "") => {
+  const postDirectory = postKey.split("/");
+  const isSeries = postDirectory[0] === "series";
+
+  if (isSeries) {
+    return postDirectory[1];
+  }
+
+  return undefined;
+};
+
+const createTable = () => {
   const files = sync(`${CONTENT_DIR_PATH}/**/*.md*`).reverse();
 
   try {
     let fileAttributes = {};
-    let tagsTablePublished = { blog: { all: [] }, project: { all: [] } };
-    let tagsTableUnpublished = { blog: { all: [] }, project: { all: [] } };
+    let seriesTablePublished = {};
+    let seriesTableUnpublished = {};
+    let tagsTablePublished = { all: [] };
+    let tagsTableUnpublished = { all: [] };
 
     for (const filePath of files) {
       const file = fs.readFileSync(filePath, { encoding: "utf8" });
@@ -21,37 +41,35 @@ const createPostTable = () => {
 
       const postPath = filePath.replace(`${CONTENT_DIR_PATH}/`, "");
       const postKey = postPath.replace(path.extname(filePath), "");
-      const postTags = tags.map((tag) =>
+
+      if (isIntroPost(postKey)) {
+        continue;
+      }
+      const series = getSeriesTitle(postKey);
+      const postTags = tags?.map((tag) =>
         tag.toLowerCase().replace(/\s|-/gi, "_")
       );
-      const postMenu = postPath.split("/")[0];
 
-      tagsTableUnpublished[postMenu].all = [
-        ...tagsTableUnpublished[postMenu].all,
-        postKey,
-      ];
+      tagsTableUnpublished.all = [...tagsTableUnpublished.all, postKey];
       if (published) {
-        tagsTablePublished[postMenu].all = [
-          ...tagsTablePublished[postMenu].all,
-          postKey,
-        ];
+        tagsTablePublished.all = [...tagsTablePublished.all, postKey];
       }
 
       postTags.forEach((t) => {
-        tagsTableUnpublished[postMenu][t] = tagsTableUnpublished[postMenu][t]
-          ? [...tagsTableUnpublished[postMenu][t], postKey]
+        tagsTableUnpublished[t] = tagsTableUnpublished[t]
+          ? [...tagsTableUnpublished[t], postKey]
           : [postKey];
-
         if (published) {
-          tagsTablePublished[postMenu][t] = tagsTablePublished[postMenu][t]
-            ? [...tagsTablePublished[postMenu][t], postKey]
+          tagsTablePublished[t] = tagsTablePublished[t]
+            ? [...tagsTablePublished[t], postKey]
             : [postKey];
         }
       });
+
       fileAttributes[postKey] = {
         ...attributes,
         path: postPath,
-        menu: postMenu,
+        series,
         title,
         tags: postTags,
         date: date ? new Date(date).toISOString().substring(0, 19) : "",
@@ -73,9 +91,17 @@ const createPostTable = () => {
       }),
       "utf-8"
     );
+    fs.writeFileSync(
+      path.join(TABLE_DIR_PATH, `seriesTable.json`),
+      JSON.stringify({
+        published: seriesTablePublished,
+        unpublished: seriesTableUnpublished,
+      }),
+      "utf-8"
+    );
   } catch (e) {
     console.error(e);
   }
 };
 
-createPostTable();
+createTable();
