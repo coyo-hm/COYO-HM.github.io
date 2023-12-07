f64bef4b94d7c7e3c262ff39df86a24462506ce6
COYO <dayeon.yoon@gmail.com>
2023-12-07 09:31:42 +0900

    STYLE: pagination style 수정

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -54,7 +54,7 @@ const Pagination = ({
             return (
               <Link href={getPath(i + startPage)} key={i + startPage}>
                 <motion.div
-                  className={`flex items-center justify-center w-3 font-semibold`}
+                  className={`flex items-center justify-center w-3 font-light`}
                   variants={pageVariants}
                   initial={"initial"}
                   animate={i + startPage === currPage ? "animate" : ""}
