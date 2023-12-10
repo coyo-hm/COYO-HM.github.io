27c511fb024177f3d92cd63c71f68b903e874749
COYO <dayeon.yoon@gmail.com>
2023-12-11 01:17:19 +0900

    FEAT: tag category 공백 처리

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,15 +1,17 @@
-export type categoryType = "post" | "series" | "tag";
+// export type categoryType = "post" | "series" | "tags";
+export type categoryType = "post" | "series";
 
-interface CategoryInfoNode {
+interface CategoryType {
   id: categoryType;
   link: string;
   label: string;
 }
 
-export const CATEGORY_KEYS: categoryType[] = ["post", "series", "tag"];
+// export const CATEGORY_KEYS: categoryType[] = ["post", "series", "tags"];
+export const CATEGORY_KEYS: categoryType[] = ["post", "series"];
 
-export const CATEGORY_INFO: { [key: string]: CategoryInfoNode } = {
+export const CATEGORY_INFO: { [key: string]: CategoryType } = {
   post: { id: "post", link: "/post/page/0/all", label: "BLOG" },
-  series: { id: "series", link: "/series/page/0/all", label: "SERIES" },
-  tag: { id: "tag", link: "/tag", label: "TAG" },
+  series: { id: "series", link: "/series/page/0", label: "SERIES" },
+  // tags: { id: "tags", link: "/tags", label: "TAG" },
 };
