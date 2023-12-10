ed473399fad8f97a0bbfeeffa2c77175fa0e50e7
COYO <dayeon.yoon@gmail.com>
2023-12-11 01:58:20 +0900

    DEPLOY: deploy to gh-pages

--- _next/static/brfqresZ0M5fBzieJOcVF/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog\u002F[page]\u002F[category]","\u002Fblog\u002Fpost\u002F[...slugs]","\u002Fproject\u002F[page]\u002F[category]","\u002Fproject\u002Fpost\u002F[...slugs]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
