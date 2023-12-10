d1d8be25aa7e760e8c89e2486ebeae08450de524
COYO <dayeon.yoon@gmail.com>
2023-12-11 01:24:59 +0900

    STYLE: post page 전체적인 스타일 변경

--- src/pages/post/[...slugs].tsx
+++ src/pages/post/[...slugs].tsx
@@ -2,24 +2,60 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { PostType } from "@models/post";
 import parseMarkdownToMdx from "@utils/parseMarkdown";
-import PostLayout from "@components/layout/PostLayout";
-import { TagWithCountType } from "@models/tag";
-import getAllTags from "@utils/getAllTags";
 import { getAllPosts } from "@utils/getPosts";
+import PostSEO from "@components/SEO/PostSEO";
+import metadata from "@config/index";
+import PostHeader from "@components/Post/PostHeader";
+import CustomMDX from "@components/Post/CustomMDX";
+import TagsList from "@components/Post/TagsList";
+import TableOfContents from "@components/Post/TableOfContents";
+import CommentWidget from "@components/Post/CommentWidget";
 
-const BlogPost = ({
-  post,
+const Post = ({
+  post: {
+    fields: { slug },
+    frontMatter,
+    body,
+    path,
+  },
   mdx,
-  tags,
 }: {
   post: PostType;
   mdx: MDXRemoteSerializeResult;
-  tags: TagWithCountType[];
 }) => {
-  return <PostLayout post={post} mdx={mdx} />;
+  const { key, series, title, tags, date, description, thumbnail } =
+    frontMatter;
+
+  return (
+    <>
+      <PostSEO
+        title={`${title}`}
+        description={description}
+        date={date}
+        url={metadata.siteUrl + "Post/" + slug}
+        tags={tags}
+        images={thumbnail ? [thumbnail] : []}
+      />
+      <article className={`flex flex-col`} id={"post"}>
+        <PostHeader {...frontMatter} />
+        <div
+          className={`flex flex-row flex-nowrap relative border-y border-y-blue-700 max-md:flex-col-reverse`}
+        >
+          <div
+            className={`grow shrink pr-10 pt-5 min-w-0 max-md:p-0 max-md:pb-4`}
+          >
+            <CustomMDX {...mdx} />
+            <TagsList tags={tags} slug={slug} />
+          </div>
+          <TableOfContents content={body} />
+        </div>
+        <CommentWidget />
+      </article>
+    </>
+  );
 };
 
-export default BlogPost;
+export default Post;
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const allPosts = await getAllPosts();
@@ -43,10 +79,7 @@ interface SlugsType {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slugs } = params as SlugsType;
   const allPosts = await getAllPosts();
-  const allTags = await getAllTags();
-  const post = allPosts.find(
-    (p) => p?.fields?.slug === ["post", ...slugs].join("/")
-  );
+  const post = allPosts.find((p) => p?.fields?.slug === slugs.join("/"));
 
   if (post) {
     const source = await parseMarkdownToMdx(post.body);
@@ -54,7 +87,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       props: {
         post,
         mdx: source,
-        tags: allTags,
       },
     };
   }
---
+++ src/pages/post/page/[page]/[tag].tsx
@@ -0,0 +1,116 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import PageSeo from "@components/SEO/PageSEO";
+import Pagination from "@components/Pagination";
+import PageTitle from "@components/Title/PageTitle";
+import metadata from "@config/index";
+import { CATEGORY_INFO } from "@constants/category";
+import { DEFAULT_NUMBER_OF_POST } from "@constants/post";
+import usePage from "@hooks/usePage";
+import { PostType } from "@models/post";
+import { TagWithCountType } from "@models/tag";
+import { getPosts } from "@utils/getPosts";
+import getLastPage from "@utils/getLastPage";
+import getAllTags from "@utils/getAllTags";
+import PostCard from "@components/Card/PostCard";
+import TagCapsule from "@components/Tag/TagCapsule";
+
+const Blog = ({
+  posts,
+  allTags,
+  selectedTag,
+  page,
+}: {
+  posts: PostType[];
+  allTags: TagWithCountType[];
+  selectedTag: string;
+  page: number;
+}) => {
+  const isAllTag = selectedTag === "all";
+  const total = allTags.find(({ tag }) => tag === tag)?.count || 0;
+  const { startPage, endPage } = usePage(total, page, "list");
+  return (
+    <>
+      <PageSeo
+        title={CATEGORY_INFO.post.label}
+        description={metadata.description}
+        url={metadata.siteUrl + `blog`}
+      />
+      <main className={`flex flex-col items-center pt-14 `}>
+        {!isAllTag && (
+          <div
+            className={`italic bg-transparent border-b-2 border-black dark:border-white mb-2 font-bold`}
+          >
+            {CATEGORY_INFO.post.label}
+          </div>
+        )}
+        <PageTitle className={`pb-2`}>
+          {isAllTag ? CATEGORY_INFO.post.label : selectedTag}
+        </PageTitle>
+        <div className={`my-12 flex flex-wrap gap-2 `}>
+          {allTags.map((tag) => (
+            <TagCapsule
+              key={tag.tag}
+              selectedTag={selectedTag}
+              category={CATEGORY_INFO.post.id}
+              {...tag}
+            />
+          ))}
+        </div>
+        <div className={`text-neutral-500 text-sm text-right w-full`}>
+          포스트 수: {total}
+        </div>
+        <article
+          className={`w-full grid gap-5 grid-cols-1 md:grid-cols-2 items-stretch my-5`}
+        >
+          {posts.map(({ frontMatter, fields: { slug } }, idx) => (
+            // <ArticleCard {...frontMatter} slug={slug} key={slug} />
+            <PostCard {...frontMatter} key={slug} slug={slug} />
+          ))}
+        </article>
+        <Pagination
+          currPage={page}
+          tag={selectedTag}
+          startPage={startPage}
+          endPage={endPage}
+          category={"post"}
+        />
+      </main>
+    </>
+  );
+};
+
+export default Blog;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allTags = await getAllTags();
+  const size = DEFAULT_NUMBER_OF_POST["list"];
+  const paths = allTags.reduce(
+    (arr: { params: { page: string; tag: string } }[], { tag, count }) => [
+      ...arr,
+      ...new Array(getLastPage(count, size)).fill(0).map((_, p) => ({
+        params: { page: "" + p, tag },
+      })),
+    ],
+    []
+  );
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page, tag } = params as { page: string; tag: string };
+  const allTags = await getAllTags();
+  const posts = await getPosts(+page, DEFAULT_NUMBER_OF_POST.list, tag);
+
+  return {
+    props: {
+      posts,
+      allTags,
+      selectedTag: tag,
+      page: +page,
+    },
+  };
+};
