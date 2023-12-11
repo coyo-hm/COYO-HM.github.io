bc5bb4429e92dcae228e713b612fa12f7fb2bb95
COYO <dayeon.yoon@gmail.com>
2023-12-11 18:30:10 +0900

    STYLE: 제목 위에 카테고리 추가

--- src/components/Post/PostHeader.tsx
+++ src/components/Post/PostHeader.tsx
@@ -1,6 +1,7 @@
 import getDate from "@utils/getDate";
 import { PostAttributeType } from "@models/post";
 import PostTitle from "@components/Title/PostTitle";
+import { CATEGORY_INFO } from "@constants/category";
 
 type PostHeaderPropsType = Pick<PostAttributeType, "title" | "date">;
 
@@ -8,6 +9,11 @@ const PostHeader = ({ title, date }: PostHeaderPropsType) => {
   const { dateStr } = getDate(date);
   return (
     <header className={`pt-28 pb-14 text-5xl font-semibold text-center`}>
+      <h1
+        className={`text-lg w-fit font-normal italic bg-transparent border-b-2 border-black dark:border-white mb-2 mx-auto`}
+      >
+        {CATEGORY_INFO.post.label}
+      </h1>
       <PostTitle className={`break-keep max-md:text-xl leading-snug`}>
         {title}
       </PostTitle>
