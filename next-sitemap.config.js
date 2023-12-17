93db15b9ce3a791cdc7526efe1d4f8848e18fe71
COYO <dayeon.yoon@gmail.com>
2023-12-18 06:28:39 +0900

    FIX: sitemap 생성 주소 변경

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,10 @@
 
 module.exports = {
   siteUrl: `https://coyo-hm.github.io/`,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 7000,
   generateRobotsTxt: true,
+  exclude: [],
+  outDir: "out",
 };
