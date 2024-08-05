93fc177e1f5093e8e11d8d7e6153bbdb20bafd84
COYO <dayeon.yoon@gmail.com>
2024-08-06 00:38:49 +0900

    CONTENT: reg-exp description 수정

--- _next/static/nhunF99Z7uQnn6RUmfT58/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fpost\u002F[...slugs]","\u002Fpost\u002Fpage\u002F[page]\u002F[tag]","\u002Fseries\u002F[title]","\u002Fseries\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
