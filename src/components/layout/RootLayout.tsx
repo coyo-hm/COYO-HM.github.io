fa60cc0f3051f5f2cba696f3d234a2ca4809c387
COYO <dayeon.yoon@gmail.com>
2024-10-21 16:23:26 +0900

    FEAT: add page routing animation

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import Header from "@components/layout/Header";
 import Footer from "@components/layout/Footer";
+import RoutingAnimation from "@components/layout/RoutingAnimation";
 
 interface Props extends PropsWithChildren {}
 
@@ -9,13 +10,15 @@ const RootLayout = ({ children }: Props) => {
     <div
       className={`bg-white dark:bg-neutral-800 text-neutral-800 dark:text-neutral-100 w-full h-screen overflow-auto`}
     >
-      <div
-        className={`bg-transparent flex flex-col justify-center px-8 m-auto min-h-screen w-full lg:w-[950px]`}
-      >
-        <Header />
-        {children}
-        <Footer />
-      </div>
+      <RoutingAnimation>
+        <div
+          className={`bg-transparent flex flex-col justify-center px-8 m-auto min-h-screen w-full lg:w-[950px]`}
+        >
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </RoutingAnimation>
     </div>
   );
 };
