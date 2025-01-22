6c1fc74cc604d914ca6be2fd569384b10a4a01be
coyo-hm <coyo-hm@users.noreply.github.com>
2025-01-22 07:45:28 +0000

    Deploying to gh-pages from @ coyo-hm/COYO-HM.github.io@2a75febd26c1a5f38ea882370fbef7db07462040 🚀

---
+++ _next/static/v9CVULiVks7S1Mj1Otefg/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fpost\u002F[id]","\u002Fpost\u002Fpage\u002F[page]\u002F[tag]","\u002Fseries\u002F[id]","\u002Fseries\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
