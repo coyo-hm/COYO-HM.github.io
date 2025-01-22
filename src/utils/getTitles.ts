a39e21e896d1289db03dfd74005064210754a7e5
COYO <dayeon.yoon@gmail.com>
2025-01-22 20:09:53 +0900

    FIX(markdown): 코드 주석 타이틀 분류 버그 수정

--- src/utils/getTitles.ts
+++ src/utils/getTitles.ts
@@ -1,16 +1,30 @@
 import parseHeadingToId from "@utils/parseHeadingToId";
 
-const getTitles = (content: string) =>
-  content
+type TitleType = { title: string; id: string; depth: number };
+
+const getTitles = (content: string) => {
+  let isCode = false;
+  const contents = content
     .split("\n")
-    .filter((t) => t.includes("# ") && t[0] === "#")
-    .map((item) => {
+    .filter((t) => (t.includes("# ") && t[0] === "#") || t.includes("```"));
+
+  return contents.reduce((titles: TitleType[], item: string) => {
+    if (!isCode && item.includes("# ")) {
       const title = item.split("# ")[1].replace(/`|\*/g, "").trim();
-      return {
-        title,
-        id: parseHeadingToId(title),
-        depth: item.match(/#/g)?.length || 0,
-      };
-    });
+      return [
+        ...titles,
+        {
+          title,
+          id: parseHeadingToId(title),
+          depth: item.match(/#/g)?.length || 0,
+        },
+      ];
+    }
+    if (item.includes("```")) {
+      isCode = !isCode;
+    }
+    return titles;
+  }, []);
+};
 
 export default getTitles;
